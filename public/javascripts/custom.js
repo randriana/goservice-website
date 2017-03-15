12b4704fb65edecb52ed8ceb64af1903ceb5e863
randriana <rina.andriana@outlook.com>
2017-03-15 14:07:03 +0100

    finished with widget animation i think

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -4,32 +4,44 @@
 
     /* move line when clicking button */
     $('.widget-button').click(function(e) {
+
+        e.preventDefault();
+
+        var self = this;
+
+        setTimeout(function(){
+            window.location.href = self.href;
+        }, 1000);
+
         var newPos = e.target.offsetLeft +  e.target.offsetWidth/2;
 
-        $('.widget-first-line').css({
-            '-webkit-transform': 'translateX(' + newPos + 'px)',
-            '-moz-transform': 'translateX(' + newPos + 'px)',
-            '-ms-transform': 'translateX(' + newPos + 'px)',
-            '-o-transform': 'translateX(' + newPos + 'px)',
-            'transform': 'translateX(' + newPos + 'px)'
-        });
+        $('.widget-first-line').addClass('short').on('webkitTransitionEnd oTransitionEnd otransitionend transitionend', function(){
 
-        $('.widget-content-container').addClass('middle-out');
+            $('.widget-first-line').css({
+                '-webkit-transform': 'translateX(' + newPos + 'px)',
+                '-moz-transform': 'translateX(' + newPos + 'px)',
+                '-ms-transform': 'translateX(' + newPos + 'px)',
+                '-o-transform': 'translateX(' + newPos + 'px)',
+                'transform': 'translateX(' + newPos + 'px)'
+            });
+            
+            $('.widget-first-line').removeClass('short');
+        })
+
+        
+
+        if($('.widget-container').hasClass("active") === false){
+            /* add middle-out-anim for in the beginning */
+            $('.widget-content-container').addClass('middle-out middle-out-anim').on('animationend animationend webkitAnimationEnd oanimationend MSAnimationEnd', function(){
+                $('.widget-content-container').removeClass('middle-out-anim')
+            });
+
+            $('.widget-container').addClass('active');
+        } else {
+            $('.widget-content-container').addClass('short-anim')
+            .on('animationend animationend webkitAnimationEnd oanimationend MSAnimationEnd', function(){
+                $('.widget-content-container').removeClass('short-anim')
+            })
+        }
     });
-
-    /* move line when clicking button 
-    $('.widget-button').click(function(e) {
-        var newPos = e.target.offsetLeft +  e.target.offsetWidth/2;
-
-        $('.widget-first-line').css({
-            '-webkit-transform': 'translateX(' + newPos + 'px)',
-            '-moz-transform': 'translateX(' + newPos + 'px)',
-            '-ms-transform': 'translateX(' + newPos + 'px)',
-            '-o-transform': 'translateX(' + newPos + 'px)',
-            'transform': 'translateX(' + newPos + 'px)'
-        });
-
-        $('.widget-middle-line').addClass('middle-out');
-    });*/
-
 })();
