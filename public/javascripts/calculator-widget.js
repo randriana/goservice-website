381644b1057fc3c0342ac39c89af0232c65e319c
randriana <rina.andriana@outlook.com>
2017-03-17 20:00:50 +0100

    done with calculator v0.5

--- public/javascripts/calculator-widget.js
+++ public/javascripts/calculator-widget.js
@@ -1,6 +1,63 @@
 new Vue({
     el: '#calculator-section',
     data: {
-        test: 'hallo test',
+        totalPrice: 0,
+        numberOfKvm: 0,
+        serviceType: '',
+        periodicService: '',
+        activeQuestion: 'servicesQuestion',
+        windowSide: {
+            inside: false,
+            outside: false
+        },
+        gulvbehandling: {
+            boning: false,
+            skuring: false
+        },
+        byggEkstra: {
+            silicon: false,
+            paint: false
+        },
+        winter: {
+            broyting: false,
+            salting: false
+        }
+    },
+    methods: {
+        setServiceType: function(e) {
+            var serviceType = e.target.textContent;
+            this.serviceType = serviceType;
+
+            if(serviceType == "Periodisk vask") {
+                this.activeQuestion = 'periodicQuestion';
+            } else if(serviceType == "Gulvbehandling"){
+                this.activeQuestion = 'floorQuestion';
+            } else {
+                this.activeQuestion = 'kvmQuestion';
+            }
+        },
+        setKvm: function() {
+            this.activeQuestion = 'kvmQuestion';
+        },
+        setPeriodicService: function(e) {
+            var periodicType = e.target.textContent;
+            this.periodicService = periodicType;
+
+            if(periodicType == 'Vindusvask') {
+                this.activeQuestion = 'windowQuestion';
+            } else if(periodicType == 'Byggvask') {
+                this.activeQuestion = 'byggQuestion';
+            } else {
+                this.activeQuestion = 'kvmQuestion';
+            }
+        },
+        setExtraService: function(e) {
+            var extraType = e.target.textContent;
+
+            this.activeQuestion = 'winterQuestion';
+        },
+        finish: function() {
+            this.activeQuestion = 'finish';
+        }
     }
 });
