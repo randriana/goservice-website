838045d97e80017150b7ef97c5b28c89462125c3
randriana <rina.andriana@outlook.com>
2017-03-18 11:32:44 +0100

    done with calculator logic

--- public/javascripts/calculator-widget.js
+++ public/javascripts/calculator-widget.js
@@ -1,27 +1,21 @@
 new Vue({
     el: '#calculator-section',
     data: {
-        totalPrice: 0,
         numberOfKvm: 0,
         serviceType: '',
         periodicService: '',
+        numberOfKgSalt: '',
+        numberOfWinterHours: '',
         activeQuestion: 'servicesQuestion',
-        windowSide: {
-            inside: false,
-            outside: false
-        },
-        gulvbehandling: {
-            boning: false,
-            skuring: false
-        },
-        byggEkstra: {
-            silicon: false,
-            paint: false
-        },
-        winter: {
-            broyting: false,
-            salting: false
-        }
+
+        dagligVask: { active: false, pricePerKvm: 10 },
+        flytteVask: { active: false, pricePerKvm: 43 },
+        vindusvask: { inside: false, outside: false, pricePerSide: 12 },
+        byggvask: { active: false, silicon: false, maling: false, pricePerKvm: 19, priceSilicon: 10, priceMaling: 10 },
+        nedvask: { active: false, pricePerKvm: 43 },
+        tepperens: { active: false, pricePerKvm: 25 },
+        gulvbehandling: { boning: false, skuring: false, priceBoning: 25, priceSkuring: 20 },
+        winter: { broyting: false, salting: false, broytingPerHour: 500, saltingPerHour: 250, saltPerKg: 2 },
     },
     methods: {
         setServiceType: function(e) {
@@ -32,22 +26,40 @@ new Vue({
                 this.activeQuestion = 'periodicQuestion';
             } else if(serviceType == "Gulvbehandling"){
                 this.activeQuestion = 'floorQuestion';
-            } else {
+            } else if(serviceType == "Tilleggstjenester"){
+                this.activeQuestion = 'extraQuestion';
+            } else if(serviceType == "Daglig vask"){
+                this.dagligVask.active = true;
+                this.activeQuestion = 'kvmQuestion';
+            } else if(serviceType == "Flyttevask") {
+                this.flytteVask.active = true;
                 this.activeQuestion = 'kvmQuestion';
             }
         },
         setKvm: function() {
             this.activeQuestion = 'kvmQuestion';
         },
+        setKgSalt: function() {
+            this.activeQuestion = 'kgSaltQuestion';
+        },
+        setWinterHours: function() {
+            this.activeQuestion = 'winterHoursQuestion';
+        },
         setPeriodicService: function(e) {
             var periodicType = e.target.textContent;
             this.periodicService = periodicType;
 
             if(periodicType == 'Vindusvask') {
+                this.vindusvask.active = true;
                 this.activeQuestion = 'windowQuestion';
             } else if(periodicType == 'Byggvask') {
+                this.byggvask.active = true;
                 this.activeQuestion = 'byggQuestion';
-            } else {
+            } else if(periodicType == 'Tepperens') {
+                this.tepperens.active = true;
+                this.activeQuestion = 'kvmQuestion';
+            } else if(periodicType == 'Nedvask') {
+                this.nedvask.active = true;
                 this.activeQuestion = 'kvmQuestion';
             }
         },
@@ -58,6 +70,77 @@ new Vue({
         },
         finish: function() {
             this.activeQuestion = 'finish';
+        },
+        restart: function() {
+            this.resetData();
+            this.activeQuestion = 'servicesQuestion';
+        },
+        resetData: function() {
+            this.dagligVask.active = false;
+            this.flytteVask.active = false;
+            this.vindusvask.inside = false;
+            this.vindusvask.outside = false;
+            this.byggvask.active = false;
+            this.byggvask.silicon = false;
+            this.byggvask.maling = false;
+            this.nedvask.active = false;
+            this.tepperens.active = false;
+            this.gulvbehandling.boning = false;
+            this.gulvbehandling.skuring = false;
+            this.winter.broyting = false;
+            this.winter.salting = false;
+            this.numberOfKvm = 0;
+            this.serviceType = '';
+            this.periodicService = '';
+            this.numberOfKgSalt = 0;
+            this.numberOfWinterHours = 0;
+        }
+    },
+
+    computed: {
+        totalPrice: function() {
+            var total = 0;
+
+            if(this.vindusvask.inside) {
+                total += this.vindusvask.pricePerSide*this.numberOfKvm;
+            } 
+            if(this.vindusvask.outside) {
+                total += this.vindusvask.pricePerSide*this.numberOfKvm;
+            }
+            if(this.dagligVask.active) {
+                total += this.dagligVask.pricePerKvm*this.numberOfKvm;
+            }
+            if(this.byggvask.active) {
+                total += this.byggvask.pricePerKvm*this.numberOfKvm;
+            }
+            if(this.byggvask.silicon) {
+                total += this.byggvask.priceSilicon*this.numberOfKvm;
+            }
+            if(this.byggvask.maling) {
+                total += this.byggvask.priceMaling*this.numberOfKvm;
+            }
+            if(this.nedvask.active) {
+                total += this.nedvask.pricePerKvm*this.numberOfKvm;
+            }
+            if(this.tepperens.active) {
+                total += this.tepperens.pricePerKvm*this.numberOfKvm;
+            }
+            if(this.gulvbehandling.boning) {
+                total += this.gulvbehandling.priceBoning*this.numberOfKvm;
+            }
+            if(this.gulvbehandling.skuring) {
+                total += this.gulvbehandling.priceSkuring*this.numberOfKvm;
+            }
+            if(this.flytteVask.active) {
+                total += this.flytteVask.pricePerKvm*this.numberOfKvm;
+            }
+            if(this.winter.broyting) {
+                total += this.winter.broytingPerHour*this.numberOfWinterHours;
+            }
+            if(this.winter.salting) {
+                total += this.winter.saltingPerHour*this.numberOfWinterHours + this.numberOfKgSalt*this.winter.saltPerKg;
+            }
+            return total;
         }
     }
 });
