b21dd3bdb7209145697ea69e41e94814343d339b
randriana <rina.andriana@outlook.com>
2017-03-26 19:20:35 +0200

    done with fancy navbar

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -3,9 +3,6 @@
 /* rellax instance */
 var rellax = new Rellax('.rellax');
 
-
-
-
  /* run widget anim automatically */
     var animActive = false;
 
@@ -18,6 +15,33 @@ var rellax = new Rellax('.rellax');
             animActive = true;          
         }
     }
+
+    $(window).scroll(function() {
+        var scrollPos = $(this).scrollTop();
+        var heroHeight = $('.video-container').height();
+        var navHeight = $('nav').height();
+        var variable = heroHeight/100;
+        var opacity = scrollPos/variable/100;
+        var offset = 150;
+
+        if(scrollPos > offset) {
+            $('.video-overlay').addClass('less-opacity');
+
+            $('.video-overlay-gradient').css({
+                'background':'linear-gradient(rgba(61, 72, 83,' + opacity + ') 20%, rgba(61, 72, 83, ' + (opacity + 0.3) + ') 100%)',
+                'visibility': 'visible'
+            });
+        } else {
+            $('.video-overlay-gradient').css('visibility', 'hidden')
+            $('.video-overlay').removeClass('less-opacity');
+        }
+
+        if(scrollPos >  heroHeight - navHeight) {
+            $('nav').addClass('no-transparent');
+        } else {
+            $('nav').removeClass('no-transparent');
+        }
+    })
 
 
 
