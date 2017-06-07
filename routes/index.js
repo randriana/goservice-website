9be6f37387c6bd5cf3c6c83726e3e5d21a135e0b
randriana <rina.andriana@outlook.com>
2017-06-07 22:24:16 +0200

    no cache

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.writeHead(200, {'Cache-Control': 'no-cache'});
   res.render('index', { title: 'Express' });
 });
 
